34379e1ba64070216d67f096cbfdcc29cda750d1
Avilin <avilin.persson@gmail.com>
2021-09-30 14:19:36 +0200

    added plugin with content text

--- wp-content/plugins/labb-2-style/index.php
+++ wp-content/plugins/labb-2-style/index.php
@@ -2,10 +2,22 @@
 
 /* 
 Plugin Name: Labb 2 Plugin
-Description:  
+Description: An awesome plugin  
 Version: 1.0
+Author: Avilin Persson
+Author URI: http://www.mywebsite.com
 
 */
 
+?>
+
+
+<?php 
+
+add_action( 'the_content', 'my_thank_you_text' );
+
+function my_thank_you_text ( $content ) {
+    return $content .= '<p>Thank you for reading this post!</p>';
+}
 
 ?>
